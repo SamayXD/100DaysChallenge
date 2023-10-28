8e7a058369b916c641dab6d1b5221bede549dc66
SamayXD <samaynavale03@gmail.com>
2023-10-29 00:55:26 +0530

    Day 1 (Done) -> Printing, Inputting, Basic Debugging, etc

---
+++ Python/Day_1/band_name_gen.py
@@ -0,0 +1,14 @@
+#Day_1 Project
+
+#1. Create a greeting for your program.
+print("Welcome to Band name Generator :D")
+
+#2. Ask the user for the city that they grew up in.
+livinCity = input("Which City you grew up in?\n=> ")
+#3. Ask the user for the name of a pet.
+petName = input("What is the name of your pet?\n=> ")
+
+#4. Combine the name of their city and pet and show them their band name.
+bandName = livinCity + " " + petName
+
+print(f"\nHere's a name for your new band: {bandName}")
---
+++ Python/Day_1/main.py
@@ -0,0 +1,21 @@
+#Day_1
+
+#===============================================
+# name = input("What is Your name? ")
+# print(f"Hello {name}!")
+# print(len(name))
+#===============================================
+
+
+#===============================================
+#Switching 2 var
+
+num1 = input("a: ")
+num2 = input("b: ")
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print(f"New a = {num1}\nNew b = {num2}")
+#===============================================
